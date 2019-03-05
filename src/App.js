9386fc465a3704568eaf82c6c10c0c5f9b3697c8
unknown <ioannis.agalliadis@rwth-aachen.de>
2019-03-05 20:56:47 +0200

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      colors: [{ name: "" }]
+    };
+  }
+  handleAddColor = () => {
+    this.setState({
+      colors: this.state.colors.concat([{ name: "" }])
+    });
+  };
+
+  handleRemoveColor = idx => () => {
+    this.setState({
+      colors: this.state.colors.filter((s, sidx) => idx !== sidx)
+    });
+  };
+
+  handleColorNameChange = idx => evt => {
+      const newColors = this.state.colors.map((colorHex, sidx) => {
+        if (idx !== sidx) return colorHex;
+        return { ...colorHex, name: evt.target.value };
+      });
+      this.setState({ colors: newColors });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form>
+        <h4 className="title">Colors</h4>
+        {this.state.colors.map((colorHex, idx) => (
+          <div className="textinput">
+            <input
+              type="text"
+              placeholder={`HEX Color #${idx + 1}`}
+              value={colorHex.name}
+              onChange={this.handleColorNameChange(idx)}
+            />
+            <button
+              className="small"
+              type="button"
+              onClick={this.handleRemoveColor(idx)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+        <button className="small" type="button" onClick={this.handleAddColor}>
+          Add color
+        </button>
+      </form>
     );
   }
 }
-
 export default App;
